91813bac57e5702f10bbd3844a07706890cf85e8
Bailey Cage <bailey.cage@gmail.com>
2023-02-13 12:21:58 -0500

    implement oxford_comma() function

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    combo = ""
+    if len(items) >= 3:
+        last = items.pop()
+        combo = ", ".join(items)
+        combo += f", and {last}"
+    elif len(items) == 2:
+        combo = items[0] + " and " + items[1]
+    else:
+        combo = items.pop()
+    return combo
